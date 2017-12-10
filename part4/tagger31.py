ed5a6afe7cdaaa8c844a4407adb54574ece1fe97
Tamir Moshiashvili <norblet82@gmail.com>
2017-12-11 01:35:55 +0200

    # half of part 4, without the pre trained vectors

---
+++ part4/tagger31.py
@@ -0,0 +1,279 @@
+import os
+from StringIO import StringIO
+from time import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as tu_data
+from torch.autograd import Variable
+
+import utils41 as utils
+
+I2W = utils.I2W
+prefix_dict = utils.prefix_dict
+suffix_dict = utils.suffix_dict
+UNK = utils.UNK
+END = utils.END
+AFFIX_LENGTH = utils.AFFIX_LENGTH
+
+
+def get_prefix(word):
+    if word not in I2W:
+        return prefix_dict[I2W[UNK]]
+    word = I2W[word]
+    if word == UNK or word == END or len(word) <= AFFIX_LENGTH:
+        return prefix_dict[word]
+    return prefix_dict[word[:AFFIX_LENGTH]]
+
+
+def get_suffix(word):
+    if word not in I2W:
+        return suffix_dict[I2W[UNK]]
+    word = I2W[word]
+    if word == UNK or word == END or len(word) <= AFFIX_LENGTH:
+        return suffix_dict[word]
+    return suffix_dict[word[-AFFIX_LENGTH:]]
+
+
+class Net(nn.Module):
+    def __init__(self, d_in, d_out, lr, mode, iter_num, d_hid=64, embedding_dim=50, win_size=5):
+        super(Net, self).__init__()
+        self.embeddings = nn.Embedding(d_in, embedding_dim)
+        self.E_pre = nn.Embedding(d_in, embedding_dim)
+        self.E_suff = nn.Embedding(d_in, embedding_dim)
+        self.linear1 = torch.nn.Linear(win_size * embedding_dim, d_hid)
+        self.linear2 = torch.nn.Linear(d_hid, d_out)
+
+        self.emb_dim = embedding_dim
+        self.window_size = win_size
+
+        self.mode = mode
+        self.O_id = ''
+        if MODE == 'NER':
+            self.O_id = utils.tags_dict['O']
+
+        # train parameters
+        self.batch_size = 1000
+        self.lr = lr
+        self.iter_num = iter_num
+
+    def forward(self, inputs):
+        prefixes, suffixes = inputs.data.numpy().copy(), inputs.data.numpy().copy()
+        prefixes, suffixes = prefixes.reshape(-1), suffixes.reshape(-1)
+
+        for i, prefix in enumerate(prefixes):
+            prefixes[i] = get_prefix(prefix)
+        for i, suffix in enumerate(suffixes):
+            suffixes[i] = get_suffix(suffix)
+
+        prefixes, suffixes = torch.from_numpy(prefixes.reshape(inputs.data.shape)), torch.from_numpy(
+            suffixes.reshape(inputs.data.shape))
+        prefixes, suffixes = prefixes.type(torch.LongTensor), suffixes.type(torch.LongTensor)
+        prefixes, suffixes = Variable(prefixes), Variable(suffixes)
+
+        embeds = (self.embeddings(inputs) + self.E_pre(prefixes) + self.E_suff(suffixes)).view(
+            (-1, self.window_size * self.emb_dim))
+        out = self.linear2(F.tanh(self.linear1(embeds)))
+        return out
+
+    def _get_loader_of(self, data):
+        """
+        NOTE - this function is for train and dev loaders.
+        :param data: list of tuples, each tuple is (words, tag) where words is a list of 5 words (all encoded)
+        :return: DataLoader object with the given data
+        """
+        x, y = zip(*data)
+        x, y = torch.LongTensor(x), torch.LongTensor(y)
+        x, y = x.type(torch.LongTensor), y.type(torch.LongTensor)
+        data_set = tu_data.TensorDataset(x, y)
+        loader = tu_data.DataLoader(data_set, batch_size=self.batch_size, shuffle=True)
+        return loader
+
+    def _get_test_loader(self, data):
+        """
+        NOTE - this function is for test loader.
+        :param data: list of words, each is a list of 5 words (all encoded)
+        :return: DataLoader object with the given data
+        """
+        data = torch.LongTensor(data)
+        y = torch.ones(data.shape[0], 1)
+        y = y.type(torch.LongTensor)
+        data, y = data.type(torch.LongTensor), y.type(torch.LongTensor)
+        test_set = tu_data.TensorDataset(data, y)
+        loader = tu_data.DataLoader(test_set, batch_size=1, shuffle=False)
+        return loader
+
+    @staticmethod
+    def _write_to_file(fname, text):
+        """
+        write the content of text in the file.
+        :param fname: name of file to write into.
+        :param text: StringIO object, contains the content wo write.
+        """
+        f = open(fname, 'w')
+        f.write(text.getvalue())
+        f.close()
+
+    def train_on(self, train_data, dev_data, result_filename):
+        """
+        train the net on the given data and write the results of the dev set in a file (CSV format).
+        :param train_data: list of windows, each is list of 5 words.
+        :param dev_data: list of windows, each is list of 5 words.
+        :param result_filename: name of file to write the result in.
+        """
+        dev = utils.to_windows(dev_data)
+        train_loader = self._get_loader_of(train_data)
+        loader_size = len(train_loader)
+
+        criterion = nn.CrossEntropyLoss()  # include the softmax
+        optimizer = optim.Adam(self.parameters(), lr=self.lr)
+
+        text = StringIO()
+
+        for epoch in range(self.iter_num):
+            total_loss = 0.0
+            good = bad = 0.0
+            curr_t = time()
+
+            for i, data in enumerate(train_loader, 0):
+                inputs, tags = data
+                inputs, tags = Variable(inputs), Variable(tags)
+
+                optimizer.zero_grad()
+
+                outputs = self(inputs)
+                loss = criterion(outputs, tags)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.data[0]
+
+                # extract the predicted label and update good and bad
+                _, predicted = torch.max(outputs.data, 1)
+                bad += (predicted != tags.data).sum()
+                if self.mode == 'NER':
+                    zipped = zip(predicted, tags.data)
+                    good += sum([pred == real for pred, real in zipped if pred == real and pred != self.O_id])
+                else:  # POS
+                    good += (predicted == tags.data).sum()
+
+            print str(epoch) + ' - loss: ' + str(total_loss / loader_size) + ', time: ' + str(
+                time() - curr_t) + ', accuracy: ' + str(good / (good + bad))
+
+            # check loss and accuracy of dev
+            text.write(model.predict_and_check_accuracy(dev, criterion))
+        # write to file
+        self._write_to_file(result_filename, text)
+
+    def predict_and_check_accuracy(self, data_set, criterion):
+        """
+        :param data_set: list of windows, each is a list of 5 words.
+        :param criterion: loss function.
+        :return: string that represent performance, which is 'loss,accuracy'
+        """
+        good = bad = 0.0
+        total_loss = 0.0
+
+        data_loader = self._get_loader_of(data_set)
+        loader_size = len(data_loader)
+        for i, data in enumerate(data_loader, 0):
+            # predict
+            inputs, tags = data
+            inputs, tags = Variable(inputs), Variable(tags)
+            outputs = self(inputs)
+            total_loss += criterion(outputs, tags).data[0]
+            _, predicted = torch.max(outputs.data, 1)
+
+            # accuracy
+            bad += (predicted != tags.data).sum()
+            if self.mode == 'NER':
+                zipped = zip(predicted, tags.data)
+                good += sum([pred == real for pred, real in zipped if pred == real and pred != self.O_id])
+            else:  # POS
+                good += (predicted == tags.data).sum()
+        # loss, acc
+        return str(total_loss / loader_size) + ',' + str(good / (good + bad)) + '\n'
+
+    def predict_test(self, test_lines, dest_filename):
+        """
+        predict on the given test_set, write the results in a file in a format of 'word tag'.
+        :param test_lines: test set
+        :param dest_filename: name of destination file to write the results into.
+        """
+        text = StringIO()
+        # id_to_word = utils.I2W
+        id_to_tag = utils.I2T
+
+        test_loader = self._get_test_loader(utils.test_to_windows(utils.get_test_set(test_lines)))
+        for i, data in enumerate(test_loader, 0):
+            inputs, _ = data
+
+            # predict
+            outputs = self(Variable(inputs))
+            _, predicted = torch.max(outputs.data, 1)
+            tag = id_to_tag[predicted[0]]
+
+            text.write(tag + '\n')
+
+        words_and_tags = StringIO()
+        tags = text.getvalue().splitlines()
+        j = 0
+        for i in range(len(tags)):
+            while test_lines[j] == '':
+                words_and_tags.write('\n')
+                j += 1
+            words_and_tags.write(test_lines[j] + ' ' + tags[i] + '\n')
+            j += 1
+
+        # write results to file
+        self._write_to_file(dest_filename, words_and_tags)
+
+
+POS_MODEL_PATH = 'pos_model_file'
+NER_MODEL_PATH = 'ner_model_file'
+
+if __name__ == '__main__':
+    MODE = 'POS'
+    MODEL_PATH = DEV = TRAIN = TEST = ''
+    log_filename = ''
+    learning_rate = iter_number = 0
+
+    if MODE == 'POS':
+        utils.fill_data_structures(utils.train_pos_lines)
+        MODEL_PATH = POS_MODEL_PATH
+        TRAIN = utils.POS_TRAIN
+        DEV = utils.POS_DEV
+        TEST = utils.POS_TEST
+        log_filename = 'pos_dev_log_file'
+        learning_rate = 0.001
+        iter_number = 10
+    else:  # NER
+        utils.fill_data_structures(utils.train_ner_lines)
+        MODEL_PATH = NER_MODEL_PATH
+        TRAIN = utils.NER_TRAIN
+        DEV = utils.NER_DEV
+        TEST = utils.NER_TEST
+        log_filename = 'ner_dev_log_file'
+        learning_rate = 0.01
+        iter_number = 10
+
+    print 'start'
+    t = time()
+
+    vocab_size = len(utils.WORDS)
+    labels_size = len(utils.TAGS)
+
+    model = Net(vocab_size, labels_size, learning_rate, MODE, iter_number)
+    if not os.path.isfile(MODEL_PATH):  # first time
+        # train
+        model.train_on(utils.to_windows(TRAIN), DEV, log_filename)
+        # save the net after train
+        torch.save(model.state_dict(), MODEL_PATH)
+    else:  # model file exists, was trained before
+        model.load_state_dict(torch.load(MODEL_PATH))
+
+    print 'predict test'
+    model.predict_test(TEST, 'test4.' + MODE.lower())
+
+    print time() - t
---
+++ part4/utils41.py
@@ -0,0 +1,204 @@
+TAGS = set()
+WORDS = set()
+I2T = dict()
+I2W = dict()
+words_dict = dict()
+tags_dict = dict()
+
+PREFIXES = set()
+prefix_dict = dict()
+SUFFIXES = set()
+suffix_dict = dict()
+
+
+def read_file(filename):
+    """
+    :param filename name of file to read
+    :return list of lines.
+    """
+    f = open(filename, 'r')
+    lines = f.read().splitlines()
+    f.close()
+    return lines
+
+
+# unique tokens
+UNK = 'UUUNKKK'
+START = '<s>'
+END = '</s>'
+AFFIX_LENGTH = 3
+
+
+def fill_data_structures(lines):
+    """
+    fill the WORDS and TAGS sets from the given lines
+    :param lines: list of lines.
+    """
+    # include the unique tokens
+    WORDS.update({START, END, UNK})
+    TAGS.update({START, END})
+    PREFIXES.update({START, END, UNK})
+    SUFFIXES.update({START, END, UNK})
+
+    for line in lines:
+        if line == '':  # ignore empty lines
+            continue
+
+        word, tag = line.split()
+        WORDS.add(word)
+        TAGS.add(tag)
+
+        if len(word) < AFFIX_LENGTH + 1:
+            PREFIXES.add(word)
+            SUFFIXES.add(word)
+        else:
+            PREFIXES.add(word[:AFFIX_LENGTH])
+            SUFFIXES.add(word[- AFFIX_LENGTH:])
+    # dicts for encode and decode the words and tags
+    I2W.update({i: word for i, word in enumerate(WORDS)})
+    words_dict.update({word: i for i, word in enumerate(WORDS)})
+    I2T.update({i: tag for i, tag in enumerate(TAGS)})
+    tags_dict.update({tag: i for i, tag in enumerate(TAGS)})
+
+    prefix_dict.update({prefix: i for i, prefix in enumerate(PREFIXES)})
+    suffix_dict.update({suffix: i for i, suffix in enumerate(SUFFIXES)})
+
+
+def get_data_set(file_lines):
+    """
+    NOTE -  file_lines must include the tag along with the word,
+            this function is for train and dev data.
+    :param file_lines: list of lines, each line is 'word tag' or ''
+    :return: list of tuples, each tuple is sentence and its tags.
+    """
+    sentence = []
+    sentence_tags = []
+    lines = []
+
+    file_lines.append('')
+    for line in file_lines:
+        if line == '':
+            # start tags
+            sentence.insert(0, START)
+            sentence.insert(0, START)
+            sentence_tags.insert(0, START)
+            sentence_tags.insert(0, START)
+
+            # end tags
+            sentence.extend([END, END])
+            sentence_tags.extend([END, END])
+
+            # insert the current tuple (sentence, tags) and clear the lists
+            lines.append((sentence, sentence_tags))
+            sentence = []
+            sentence_tags = []
+        else:
+            # add the word and tag
+            word, tag = line.split()
+            sentence.append(word)
+            sentence_tags.append(tag)
+    return lines
+
+
+def get_test_set(file_lines):
+    """
+    NOTE -  each line in file_lines is a single word,
+            this function is for test data.
+    :param file_lines: list of lines, each line is 'word' or ''
+    :return: list of sentences.
+    """
+    sentence = []
+    lines = []
+
+    file_lines.append('')
+    for line in file_lines:
+        if line == '':
+            # start tags
+            sentence.insert(0, START)
+            sentence.insert(0, START)
+
+            # end tags
+            sentence.extend([END, END])
+
+            # insert the current tuple (sentence, tags) and clear the lists
+            lines.append(sentence)
+            sentence = []
+        else:
+            sentence.append(line)
+    return lines
+
+
+# TRAIN
+train_pos_filename = '../data/pos/train'
+train_pos_lines = read_file(train_pos_filename)
+train_ner_filename = '../data/ner/train'
+train_ner_lines = read_file(train_ner_filename)
+
+# DEV
+dev_pos_filename = '../data/pos/dev'
+dev_pos_lines = read_file(dev_pos_filename)
+dev_ner_filename = '../data/ner/dev'
+dev_ner_lines = read_file(dev_ner_filename)
+
+# TEST
+test_pos_filename = '../data/pos/test'
+POS_TEST = read_file(test_pos_filename)
+test_ner_filename = '../data/ner/test'
+NER_TEST = read_file(test_ner_filename)
+
+# create the sets
+POS_TRAIN = get_data_set(train_pos_lines)
+POS_DEV = get_data_set(dev_pos_lines)
+
+NER_TRAIN = get_data_set(train_ner_lines)
+NER_DEV = get_data_set(dev_ner_lines)
+
+
+def to_windows(data_set):
+    """
+    NOTE - data_set must include the tags along side the words,
+            this function is for train and dev data sets.
+    :param data_set: list of tuples, each tuple is (words, tags) where each of them is a list
+    :return: list of tuples, each tuple is (window, tag) where window is a vector of 5 words,
+             each element is represented by its id i.e. number.
+    """
+    windows = []
+    for words, tags in data_set:
+        for i in xrange(2, len(words) - 2):
+            # create window of words
+            curr_window = [words[i - 2], words[i - 1], words[i], words[i + 1], words[i + 2]]
+
+            # replace the unknown words in UNK
+            for j, word in enumerate(curr_window):
+                if word not in WORDS:
+                    curr_window[j] = UNK
+
+            # encode
+            curr_window = [words_dict[word] for word in curr_window]
+            windows.append((curr_window, tags_dict[tags[i]]))
+    return windows
+
+
+def test_to_windows(test_set):
+    """
+    NOTE - test_set must be *only* words,
+            this function is for test set.
+    :param test_set: list of words where each of them is a list
+    :return: list of windows, each window is a vector of 5 words,
+             each element is represented by its id i.e. number.
+    """
+    windows = []
+    for words in test_set:
+        for i in xrange(2, len(words) - 2):
+            # create window of words
+            curr_window = [words[i - 2], words[i - 1], words[i], words[i + 1], words[i + 2]]
+
+            # replace the unknown words in UNK
+            for j, word in enumerate(curr_window):
+                if word not in WORDS:
+                    curr_window[j] = UNK
+
+            # encode
+            curr_window = [words_dict[word] for word in curr_window]
+            windows.append(curr_window)
+    return windows
